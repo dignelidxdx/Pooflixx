1fb097e260f3119dcc0e771c061dcdfdfef834d0
dignelidxdx <digneli14@gmail.com>
2020-04-04 12:42:24 -0300

    Se agrego una nueva subclase Websodio

--- src/app/Websodio.java
+++ src/app/Websodio.java
@@ -10,4 +10,12 @@ public class Websodio extends Capitulo {
         System.out.println("Reproduciendo websodio " + this.nombre + this.url);
     }
 
+     /**
+     * "envia" un mail de alerta cuando la serie este online. Este metodo NO esta en
+     * EPISODIO.
+     */
+    public void enviarAlertaDeQueEstaOnline() {
+        // mandaria un mail a los usuarios avisando que ya esta disponible para ver.
+        System.out.println("Enviando mail con url " + this.url);
+    }
 }
