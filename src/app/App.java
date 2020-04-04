7c8c6dc7e17c1209221a07e2865c9dbff9791f35
dignelidxdx <digneli14@gmail.com>
2020-04-04 12:40:11 -0300

    Juntar otro codigo listo del profe con el mio en APP

--- src/app/App.java
+++ src/app/App.java
@@ -18,33 +18,60 @@ import java.util.Scanner;
 
 public class App {
 
-    public static Scanner Teclado = new Scanner(System.in);
-    public static void main(String[] args) throws Exception {
-		
+	public static Scanner Teclado = new Scanner(System.in);
 
-		Pooflix miPooflix = new Pooflix(); 
+	public static void main(String[] args) throws Exception {
 
-		System.out.println("Iniciando Catalogo"); 
-		
-		miPooflix.inicializarCatalogo(); 
-		
-		Serie bb = miPooflix.buscarSerie("The Walking Dead"); 
-		
-		Temporada laMejorTemporada = bb.getTemporada(3); 
-		
-		Capitulo elTop; 
-		
-		elTop = laMejorTemporada.getEpisodio(3); 
+		Pooflix miPooflix = new Pooflix();
 
+		System.out.println("Iniciando Catalogo....");
 
+		miPooflix.inicializarCatalogo();
 
-		
-		elTop.reproducir(); 
+		Serie bb = miPooflix.buscarSerie("The Walking Dead");
 
+		Temporada laMejorTemporada = bb.getTemporada(3);
 
-	
-	}  
+		Capitulo epiDemo;
 
-	
-	
+		int posicion = 0;
+
+		/// En base a un numero de posicion de episodio, obtenerlo de la lista
+		System.out.println("Ingrese la posicion");
+
+		posicion = Teclado.nextInt();
+
+		epiDemo = laMejorTemporada.getEpisodioAtPosicion(posicion);
+
+		epiDemo.reproducir();
+
+		// instanceof: devuelve si la variable es de tipo Clase X
+		// En este caso, devuelve si epiDemo declarado como Episodio apunta a una clase
+		// Websodio
+		if (epiDemo instanceof Websodio) {
+			System.out.println("Estoy reproduciendo un websodio");
+
+			// (clase): Operador casteo.
+			// Casteo lo que hace es acomodar la variable a castear, a una clase especifica
+			// da error si no se puede acomodar. por eso siempre se pregunta antes con un
+			// instance of
+			Websodio webso = (Websodio) epiDemo;
+			webso.enviarAlertaDeQueEstaOnline();
+		} else
+			System.out.println("Estoy reproduciendo un Episodio");
+
+		Capitulo elTop;
+
+		elTop = laMejorTemporada.getEpisodio(1);
+
+		elTop.reproducir();
+
+		Capitulo otroEpi;
+
+		otroEpi = laMejorTemporada.getEpisodio(3);
+
+		otroEpi.reproducir();
+
+	}
+
 }
